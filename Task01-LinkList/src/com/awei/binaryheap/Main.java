21671aea899c66a89c2fa5ff6193dd555e6385f5
shizuwei <shizuwei99@gmail.com>
2021-03-19 20:23:40 +0800

    finish BinaryHeap

---
+++ Task01-LinkList/src/com/awei/binaryheap/Main.java
@@ -0,0 +1,19 @@
+package com.awei.binaryheap;
+
+import java.util.Arrays;
+
+/**
+ * @Description: main function
+ * @Author: Awei
+ * @Create: 2021-03-19 16:39
+ **/
+public class Main {
+    public static void main(String[] args) {
+        int[] array = new int[]{1,3,2,6,5,7,8,9,10,0};
+        BinaryHeap.upAdjust(array);
+        System.out.println(Arrays.toString(array));
+        int[] arr = new int[]{7, 1, 3, 10, 5, 2, 8, 9, 6};
+        BinaryHeap.buildHeap(arr);
+        System.out.println(Arrays.toString(arr));
+    }
+}
