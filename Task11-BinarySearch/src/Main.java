5cd23678d5994fa9a4d22e4dd80d2e6b8655528f
shizuwei <shizuwei99@gmail.com>
2021-05-02 15:39:26 +0800

    二分查找

--- Task11-BinarySearch/src/Main.java
+++ Task11-BinarySearch/src/Main.java
@@ -22,9 +22,8 @@ public class Main {
         int left = 0, right = nums.length - 1;
         while (left <= right) {
             int mid = left + (right - left) / 2;
-            if (nums[mid] == target) {
+            if(nums[mid]==target)
                 return mid;
-            }
             if (nums[mid] < target) {
                 left = mid + 1;
             }else
